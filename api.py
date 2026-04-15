7711a4a0dfc225ba6fb4835b2c58d6de807e63e3
dikshantghimire60-wq <dikshantghimire60@gmail.com>
2026-04-15 22:10:40 +0545

    Update api.py

--- api.py
+++ api.py
@@ -38,7 +38,7 @@ site_url = "https://s**********pp/"
 HTTP_REQUEST_URLS = [
     "https://9mgl01tslv.onrender.com/",
     "https://second-3sjb.onrender.com/",
-    "https://dkdkdk.onrender.com/",
+    "https://finalsubdomain.onrender.com",
     "https://subdomaintakeover-0ki4.onrender.com/",
     # Add more URLs as needed
 ]
